ef72771cf3722a0ce63306ad5d4a25c501750f05
bluuscript <tomymilla22@gmail.com>
2024-02-29 17:43:13 -0300

    modf: log del req.body

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.post('/dataflow/cotizacion', async (req, res) => {
     // Datos Formulario de Cotización de Página Web de DataFlow
     const { nombre, empresa, email, fono, servicio, problema } = req.body
-    console.log(`RUTA: /dataflow/cotizacion - body: ${req.body}`)
+    console.log('RUTA: /dataflow/cotizacion', req.body)
     const info = await transporter.sendMail({
         from: USER_MAIL,
         to: `formcontactos@gmail.com, ${email}`,
